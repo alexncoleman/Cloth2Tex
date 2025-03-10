b8c31236c78004e82c54473cd28ba95eba21c766
Alex Coleman <111077126+alexncoleman@users.noreply.github.com>
2025-03-10 17:46:41 +0000

    moving back img

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import cv2
 
 # script to test if I can read photos
 
-img = cv2.imread("inputs/front_{0}.jpg".format(int(1.0)))
+img = cv2.imread("inputs/back_{0}.jpg".format(int(1.0)))
 print(img.shape[:2])
